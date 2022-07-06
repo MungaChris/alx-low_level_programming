e63c14b286c744ce810db1d728ff1304a9bbcc5e
MungaChris <munga.chris98@gmail.com>
2022-07-06 14:09:25 -0700

    Final

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,10 +3,10 @@
  * main - print alphabets in lower case
  *
  * Description: using the main function
- * this program prints alphabets in lower case
- *Return: 0
+ * this program prints alphabets in lower case using a prototype 'main.c'
+ * Return: 0
  */
- void print_alphabet(void)
+void print_alphabet(void)
 {
 	char letter;
 
