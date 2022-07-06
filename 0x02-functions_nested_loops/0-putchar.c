6df7ee80a7eaec7b35e102193b906aebd4f4b352
MungaChris <munga.chris98@gmail.com>
2022-07-06 13:24:40 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print _putchar
+ * Description: using the main function
+ * this program prints '_putchar
+ * Return: 0
+ */
+int main(void)
+{
+	printf("_putchar);
+
+	printf(\n);
+
+	return (0);
+}
