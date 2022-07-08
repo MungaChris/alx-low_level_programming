a346b3b1d61898dba5fa8aa7bc88a3b58479e3bb
MungaChris <munga.chris98@gmail.com>
2022-07-07 18:53:03 -0700

    Corrections

---
+++ 0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print _putchar
+ *
+ * Description: using the main function
+ * this program prints _putchar
+ * return: 0
+ */
+int main(void)
+{
+	printf("_putchar");
+
+	return (0);
+}
