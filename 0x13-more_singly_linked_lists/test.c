a3e849bdea8c88827f9aae0ca3c7056ca27d2bee
Chris Munga <munga.chris98@gmail.com>
2022-08-16 11:24:10 +0300

    Correct quiz number 9

--- 0x13-more_singly_linked_lists/test.c
+++ 0x13-more_singly_linked_lists/test.c
@@ -3,123 +3,55 @@
 #include <string.h>
 #include <stdlib.h>
 
-typedef struct listint_s
-{
-    int n;
-    struct listint_s *next;
-} listint_t;
+
 /**
-//0-Print list
-// check README.md for more info
-size_t listint_len(const listint_t *h)
+ * insert_nodeint_at_index - Inserts a new node to a listint_t
+ *                              list at a given position.
+ * @head: A pointer to the address of the head
+ *          of the listint_t list.
+ * @idx: The index of the listint_t list where the new node
+ *          should be added - indices start at 0.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *          Otherwise - the address of the new node. 
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    size_t nodes = 0;
-
-    while (h)
-    {
-        nodes++;
-        h = h->next;
-    }
-    return (nodes);
-}
-int main(void)
-{
-    listint_t *head;
-    listint_t *new;
-    listint_t hello = {8, NULL};
-    size_t n;
-
-    head = &hello;
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    new->n = 9;
-    new->next = head;
-    head = new;
-    n = listint_len(head);
-    printf("-> %lu elements\n", n);
-    free(new);
-    return (0);
-}
-*/
-
-/*
-// 3. Add node at the end of a list
-/** 
- * add_nodeint_end - Adds a new node at the end
- *                      of a listint_t list.
- * 
- * @head: A pointer to the address of the
- *          head of the listint_t list. 
- * @n: The integer for the new code to contain 
- * @return: If the function fails - NULL
- *          Otherwise - the address of the new element. 
- 
-listint_t *add_nodeint_end(listint_t **head, const int n)
-{
-    listint_t *new, *last;
+    listint_t *new, *copy = *head;
+    unsigned int node;
 
     new = malloc(sizeof(listint_t));
     if (new == NULL)
         return (NULL);
-    
+
     new->n = n;
-    new->next = NULL;
 
-    if (*head == NULL)
+    if (idx == 0)
+    {
+        new->next = copy;
         *head = new;
-    
-    else
-    {
-        last = *head;
-        while (last->next != NULL)
-                last = last->next;
-        last->next = new;
+        return (new);
     }
 
-    return(head);
+    for (node = 0; node < (idx - 1); node++)
+    {
+        if (copy == NULL || copy->next == NULL)
+            return (NULL);
+        
+        copy = copy->next;
+    }
+
+    new->next = copy->next;
+    copy->next = new;
+
+    return (new);
 }
-//Test code (main function):
-int main(void)
-{
-    listint_t *head;
-
-    head = NULL;
-    add_nodeint_end(&head, 0);
-    add_nodeint_end(&head, 1);
-    add_nodeint_end(&head, 2);
-    add_nodeint_end(&head, 3);
-    add_nodeint_end(&head, 4);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 402);
-    add_nodeint_end(&head, 1024);
-    print_listint(head);
-    return (0);
-}
-*/
-
-//4 - Free listint_t list
-
 /**
- * free_listint - Frees a listint_t list.
- * 
- * @head: A pointer to the head of the listint_t to be freed. 
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-void free_listint(listint_t *head)
-{
-    listint_t *tmp;
-
-    while (head)
-    {
-        tmp = head->next;
-        free(head);
-        head = tmp;
-    }
-}
-
 int main(void)
 {
     listint_t *head;
@@ -134,7 +66,9 @@ int main(void)
     add_nodeint_end(&head, 402);
     add_nodeint_end(&head, 1024);
     print_listint(head);
-    free_listint(head);
-    head = NULL;
+    printf("-----------------\n");
+    insert_nodeint_at_index(&head, 5, 4096);
+    print_listint(head);
+    free_listint2(&head);
     return (0);
 }
